4303615e1f084d4974e96c7c646719a2df5d0f17
Gifftybabe <ulokangozi@gmail.com>
2024-10-02 08:43:21 +0100

    updated nft market

--- contracts/NftMarketPlace.sol
+++ contracts/NftMarketPlace.sol
@@ -1,79 +1,123 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MetaMintMarketplace is ERC721, Ownable(msg.sender) {
-    uint256 public nextNFTId;
+contract NFTMarketplace is ERC721 {
+    address private _owner;
+    uint256 public nextTokenId;
 
-    struct NFTListing {
+    struct Listing {
         address seller;
         uint256 price;
-        bool isAvailable;
+        bool isForSale;
     }
 
-  
-    mapping(uint256 => NFTListing) public nftListings;
+    // Mapping from token ID to its listing details
+    mapping(uint256 => Listing) public listings;
 
-    constructor() ERC721("MetaMintMarketplace", "MMM") {
-        nextNFTId = 1; 
+    // Modifier to restrict function access to the contract owner
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Caller is not the owner");
+        _;
     }
 
- 
-    function mintNFT(address to) external onlyOwner {
-        uint256 nftId = nextNFTId; 
-        nextNFTId++; 
-        _safeMint(to, nftId);
+    constructor() ERC721("NFTMarketplace", "NFTM") {
+        _owner = msg.sender; // Set the deployer as the initial owner
+        nextTokenId = 1; // Initialize token ID to 1
     }
 
- 
-    function listNFTForSale(uint256 nftId, uint256 price) external {
-        require(ownerOf(nftId) == msg.sender, "Not the NFT owner");
+    /**
+     * @dev Mint new NFTs. Only contract owner can mint.
+     * @param to Address of the receiver of the minted NFT.
+     */
+    function mint(address to) external onlyOwner {
+        uint256 tokenId = nextTokenId; // Assign current token ID
+        nextTokenId++; // Increment token ID for next mint
+
+        _safeMint(to, tokenId);
+    }
+
+    /**
+     * @dev List an NFT for sale. Only the owner of the token can list it.
+     * @param tokenId ID of the token to list for sale.
+     * @param price Sale price in wei.
+     */
+    function listForSale(uint256 tokenId, uint256 price) external {
+        require(ownerOf(tokenId) == msg.sender, "Not the token owner");
         require(price > 0, "Price must be greater than 0");
 
-        nftListings[nftId] = NFTListing({
+        listings[tokenId] = Listing({
             seller: msg.sender,
             price: price,
-            isAvailable: true
+            isForSale: true
         });
     }
 
-  
-    function purchaseNFT(uint256 nftId) external payable {
-        NFTListing memory listedNFT = nftListings[nftId];
+    /**
+     * @dev Buy an NFT that is listed for sale. Buyer sends ETH equivalent to the price.
+     * @param tokenId ID of the token to buy.
+     */
+    function buy(uint256 tokenId) external payable {
+        Listing memory listedItem = listings[tokenId];
 
-        require(listedNFT.isAvailable, "NFT not for sale");
-        require(msg.value >= listedNFT.price, "Insufficient funds");
+        require(listedItem.isForSale, "Token not for sale");
+        require(msg.value >= listedItem.price, "Insufficient funds");
 
-   
-        _transfer(listedNFT.seller, msg.sender, nftId);
+        // Transfer ownership of the NFT
+        _transfer(listedItem.seller, msg.sender, tokenId);
 
-      
-        nftListings[nftId].isAvailable = false;
+        // Mark as no longer for sale
+        listings[tokenId].isForSale = false;
 
-        payable(listedNFT.seller).transfer(msg.value);
+        // Transfer the sale price to the seller
+        payable(listedItem.seller).transfer(msg.value);
     }
 
-   
-    function delistNFT(uint256 nftId) external {
-        require(ownerOf(nftId) == msg.sender, "Not the NFT owner");
-        require(nftListings[nftId].isAvailable, "NFT is not listed for sale");
+    /**
+     * @dev Remove an NFT from being listed for sale. Only the owner can remove the listing.
+     * @param tokenId ID of the token to remove from sale.
+     */
+    function removeListing(uint256 tokenId) external {
+        require(ownerOf(tokenId) == msg.sender, "Not the token owner");
+        require(listings[tokenId].isForSale, "Token is not listed for sale");
 
-        delete nftListings[nftId];
+        delete listings[tokenId];
     }
 
-   
-    function updateNFTPrice(uint256 nftId, uint256 newPrice) external {
-        require(ownerOf(nftId) == msg.sender, "Not the NFT owner");
-        require(nftListings[nftId].isAvailable, "NFT not listed for sale");
+    /**
+     * @dev Change the price of an NFT that is already listed for sale. Only the seller can update.
+     * @param tokenId ID of the token to update price.
+     * @param newPrice The new sale price in wei.
+     */
+    function updatePrice(uint256 tokenId, uint256 newPrice) external {
+        require(ownerOf(tokenId) == msg.sender, "Not the token owner");
+        require(listings[tokenId].isForSale, "Token not listed for sale");
         require(newPrice > 0, "Price must be greater than 0");
 
-        nftListings[nftId].price = newPrice;
+        listings[tokenId].price = newPrice;
     }
 
- 
-    function withdrawFunds() external onlyOwner {
-        payable(owner()).transfer(address(this).balance);
+    /**
+     * @dev Withdraw contract's balance. Only the owner of the contract can withdraw.
+     */
+    function withdraw() external onlyOwner {
+        payable(_owner).transfer(address(this).balance);
+    }
+
+    /**
+     * @dev Returns the address of the contract owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "New owner is the zero address");
+        _owner = newOwner;
     }
 }
